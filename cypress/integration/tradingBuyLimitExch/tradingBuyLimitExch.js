b92136346d22af91ebbd4b83a878622b8d9b6dcf
gabriel0182 <gabriel0182@gmail.com>
2021-04-07 10:20:15 -0500

    Buy Limit Exchencge Trading

---
+++ cypress/integration/tradingBuyLimitExch/tradingBuyLimitExch.js
@@ -0,0 +1,29 @@
+///  <reference types="cypress"/>
+
+import login from "../PageObject/login.js";
+import buyLimitExch from "../PageObject/buyLimitExch.js";
+
+const staging = new login();
+const limitExch = new buyLimitExch();
+
+Given("I go to Trading page", () => {
+  staging.landing();
+  staging.longIn();
+});
+
+When("I go to Trading page", () => {
+    limitExch.trading();
+});
+
+When("I type the order required info", () => {
+    limitExch.orderInfo();
+});
+
+When("I select to Exchange Buy", () => {
+    limitExch.buyButton();
+});
+
+Then("I verify the limit order was created", () => {
+    limitExch.successMsg();
+    limitExch.cancelOrder();
+});
