aa35cf84a6e62edf6d4969a8165f88e777194216
gabriel0182 <gabriel0182@gmail.com>
2021-04-15 15:27:47 -0500

    Trailing stop buy exchange

---
+++ cypress/integration/PageObject/trailingStopExch.js
@@ -0,0 +1,102 @@
+class trailingStopExch {
+  trading() {
+    const tradingTab = cy.waitUntil(() =>
+      cy
+        .get(".header__nav-buttons-wrapper > .header__nav-trading")
+        .should("be.visible")
+        .click({ force: true })
+    );
+    return this;
+  }
+  orderInfo() {
+    const testData = require("../../fixtures/orders.json");
+    testData.forEach((testDataRow) => {
+      const data = {
+        wallet1: testDataRow.wallet1,
+        type4: testDataRow.type4,
+        price: testDataRow.price,
+        btc: testDataRow.btc,
+      };
+      context(`Generating a test for ${data.wallet1}`, () => {
+        const orderType = cy
+          .get(
+            ":nth-child(1) > .ui-dropdown__wrapper > .o-type-select > .ui-dropdown__buttonwrap"
+          )
+          .click({ force: true })
+          .get('[id="orderFormDropdown"]')
+          .get('[id="orderFormDropdownItem_trailingstop"]')
+          .contains(data.type4)
+          .click({ force: true });
+        const distanceUSD = cy.get('[name="price"]');
+        distanceUSD.type(data.price);
+        const amountBTC = cy.get('[name="amount"]');
+        amountBTC.type(data.btc);
+        const orderFrom = cy
+          .get("#form-choose-exchange")
+          .contains(data.wallet1);
+        orderFrom.click({ force: true });
+      });
+    });
+    return this;
+  }
+  buyButton() {
+    const exchangeBuy = cy.get("#buyButton").contains("Exchange Buy");
+    exchangeBuy.click({ force: true });
+    return this;
+  }
+  successMsg() {
+    const testData = require("../../fixtures/orders.json");
+    testData.forEach((testDataRow) => {
+      const data = {
+        price: testDataRow.price,
+        btc: testDataRow.btc,
+      };
+      context(`Generating a test for ${data.price}`, () => {
+        const msg = cy.waitUntil(() =>
+          cy
+            .get(".notification-text__text")
+            .should("be.visible")
+            .should(
+              "contain",
+              `Created exchange trailing stop buy order of ${data.btc} BTC`
+            )
+        );
+      });
+    });
+    return this;
+  }
+  cancelOrder() {
+    const ordersTable = cy
+      .get('[data-qa-id="orders-table"]')
+      .get("div")
+      .first("div")
+      .each(($div) => {
+        cy.get(
+          '[style="position: absolute; left: 0px; top: 25px; height: 25px; width: 100%; padding-right: 0px;"]'
+        )
+          .get(
+            '[style="position: absolute; left: 0px; top: 25px; height: 25px; width: 100%; padding-right: 0px;"] > [style="flex: 0 1 105px; min-width: 105px; max-width: 105px;"] > :nth-child(3) > .ui-button > .fa'
+          )
+          .click({ force: true });
+        const testData = require("../../fixtures/orders.json");
+        testData.forEach((testDataRow) => {
+          const data = {
+            btc: testDataRow.btc,
+          };
+          context(`Generating a test for ${data.btc}`, () => {
+            const msgCancel = cy.waitUntil(() =>
+              cy
+                .get(".notification-text__text")
+                .should("be.visible")
+                .should(
+                  "contain",
+                  `Exchange trailing stop buy order of ${data.btc} BTC has been canceled`
+                )
+            );
+          });
+        });
+      });
+    return this;
+  }
+}
+export default trailingStopExch;
