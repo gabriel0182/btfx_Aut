ba0a610a28f6d1e6974638d595f2c77e0129795c
gabriel0182 <gabriel0182@gmail.com>
2021-04-20 17:10:48 -0500

    Immediate or Cancel buy v1

---
+++ cypress/integration/PageObject/buyImmediateCancel.js
@@ -0,0 +1,78 @@
+class buyImmediateCancel {
+  trading() {
+    const tradingTab = cy.waitUntil(() =>
+      cy
+        .get(".header__nav-buttons-wrapper > .header__nav-trading")
+        .should("be.visible")
+        .click({ force: true })
+    );
+    return this;
+  }
+  orderInfo() {
+    const testData = require("../../fixtures/orders.json");
+    testData.forEach((testDataRow) => {
+      const data = {
+        wallet1: testDataRow.wallet1,
+        type6: testDataRow.type6,
+        price: testDataRow.price,
+        btc: testDataRow.btc,
+      };
+      context(`Generating a test for ${data.wallet1}`, () => {
+        const orderForm = cy.waitUntil(() =>
+          cy.get("#orderform-panel").should("be.visible").should("exist")
+        );
+        const orderType = cy.waitUntil(() =>
+          cy
+            .get(
+              ":nth-child(1) > .ui-dropdown__wrapper > .o-type-select > .ui-dropdown__buttonwrap"
+            )
+            .should("be.visible")
+            .scrollIntoView()
+            .click({ force: true })
+            .get("ul.dropdown-content", { force: true })
+            .within(() => {
+              cy.get("#orderFormDropdownItem_immediateorcancel")
+                .contains(data.type6)
+                .click({ force: true });
+            })
+        );
+        const distanceUSD = cy.get('[name="price"]');
+        distanceUSD.type(data.price);
+        const amountBTC = cy.get('[name="amount"]');
+        amountBTC.type(data.btc);
+        const orderFrom = cy
+          .get("#form-choose-exchange")
+          .contains(data.wallet1);
+        orderFrom.click({ force: true });
+      });
+    });
+    return this;
+  }
+  buyButton() {
+    const exchangeBuy = cy.get("#buyButton").contains("Exchange Buy");
+    exchangeBuy.click({ force: true });
+    return this;
+  }
+  successMsg() {
+    const testData = require("../../fixtures/orders.json");
+    testData.forEach((testDataRow) => {
+      const data = {
+        price: testDataRow.price,
+        btc: testDataRow.btc,
+      };
+      context(`Generating a test for ${data.price}`, () => {
+        const msg = cy.waitUntil(() =>
+          cy
+            .get(".notification-text__text")
+            .should("be.visible")
+            .should(
+              "contain",
+              `Exchange ioc buy order of ${data.btc} BTC has been fully executed`
+            )
+        );
+      });
+    });
+    return this;
+  }
+}
+export default buyImmediateCancel;
---
+++ cypress/integration/buyImmediateOrCancelExch/buyImmediateOrCancelExch.js
@@ -0,0 +1,28 @@
+///  <reference types="cypress"/>
+
+import login from "../PageObject/login.js";
+import buyImmediateCancel from "../PageObject/buyImmediateCancel.js";
+
+const staging = new login();
+const buyIC = new buyImmediateCancel();
+
+Given("I go to Trading page", () => {
+  staging.landing();
+  staging.longIn();
+});
+
+When("I go to Trading page", () => {
+    buyIC.trading();
+});
+
+When("I type the order required info", () => {
+    buyIC.orderInfo();
+});
+
+When("I select to Exchange Buy", () => {
+    buyIC.buyButton();
+});
+
+Then("I verify the Immediate or Cancel buy order from Exchange wallet was created", () => {
+    buyIC.successMsg();
+});
