48899937e360987f4cc8f895cbe340450296be68
gabriel0182 <gabriel0182@gmail.com>
2021-05-02 17:25:34 -0500

    sell scale v2

--- cypress/integration/PageObject/sellScaled.js
+++ cypress/integration/PageObject/sellScaled.js
@@ -29,7 +29,7 @@ class sellScaled {
           var amout = pointNum * 1090;
           var value = amout + 100;
           localStorage.setItem("price", value);*/
-                const lowerUSD = cy.get('#priceinput3')
+                const lowerUSD = cy.get("#priceinput3");
                 lowerUSD.type(txt1);
                 const uperUSD = cy.get("#priceinput4");
                 uperUSD.type(txt2);
@@ -50,7 +50,7 @@ class sellScaled {
                 const orderFrom = cy
                   .get("#form-choose-exchange")
                   .contains(data.wallet1);
-                orderFrom.click({ force: true });;
+                orderFrom.click({ force: true });
               }
             );
           }
@@ -60,54 +60,54 @@ class sellScaled {
     return this;
   }
   submitButton() {
-    const distribution = cy.get('#radio-upwards > .circle')
-    distribution.click({force:true})
-    const action = cy.get('#radio-sell > .circle')
-    action.click({force:true})
-    const submit = cy.get('#submitButton')
+    const distribution = cy.get("#radio-upwards > .circle");
+    distribution.click({ force: true });
+    const action = cy.get("#radio-sell > .circle");
+    action.click({ force: true });
+    const submit = cy.get("#submitButton");
     submit.click({ force: true });
     return this;
   }
   successMsg() {
-        const msg = cy.waitUntil(() =>
-          cy.get(".notification-text__text").should("be.visible")
-        );
-        const verifyMsg = cy.waitUntil(() =>
-          cy
-            .get(".notification-text__text")
-            .should(
-              "contain",
-              `Created exchange limit sell order of`
-            ).wait(5000)
-        );
+    const msg = cy.waitUntil(() =>
+      cy.get(".notification-text__text").should("be.visible")
+    );
+    const verifyMsg = cy.waitUntil(() =>
+      cy
+        .get(".notification-text__text")
+        .should("contain", `Created exchange limit sell order of`)
+    );
     return this;
   }
   cancelOrder() {
-    const ordersTable = cy
-      .get('[data-qa-id="orders-table"]')
-      .get("div")
-      .first("div")
-      .each(($div) => {
-        cy.get(
-          '[style="position: absolute; left: 0px; top: 25px; height: 25px; width: 100%; padding-right: 0px;"]'
-        )
-          .get(
-            '[style="position: absolute; left: 0px; top: 25px; height: 25px; width: 100%; padding-right: 0px;"] > [style="flex: 0 1 105px; min-width: 105px; max-width: 105px;"] > :nth-child(3) > .ui-button > .fa'
-          )
-          .click({ force: true });
-            const msgCancel = cy.waitUntil(() =>
-              cy.get(".notification-text__text").should("be.visible")
-            );
-            const verifyMsg = cy.waitUntil(() =>
-              cy
-                .get(".notification-text__text")
-                .should(
-                  "contain",
-                  `has been canceled`
+    const msg = cy.waitUntil(() =>
+      cy
+        .get(".notification-text__text")
+        .should("be.visible")
+        .get('div.notification__skip').click({ force: true })
+    )
+            const ordersTable = cy
+              .get('[data-qa-id="orders-table"]')
+              .get("div")
+              .first("div")
+              .each(($div) => {
+                cy.get(
+                  '[style="position: absolute; left: 0px; top: 25px; height: 25px; width: 100%; padding-right: 0px;"]'
                 )
-            );
-          });
-    return this;
-  }
+                  .get(
+                    '[style="position: absolute; left: 0px; top: 25px; height: 25px; width: 100%; padding-right: 0px;"] > [style="flex: 0 1 105px; min-width: 105px; max-width: 105px;"] > :nth-child(3) > .ui-button > .fa'
+                  )
+                  .click({ force: true });
+                const msgCancel = cy.waitUntil(() =>
+                  cy.get(".notification-text__text").should("be.visible")
+                );
+                const verifyMsg = cy.waitUntil(() =>
+                  cy
+                    .get(".notification-text__text")
+                    .should("contain", `has been canceled`)
+                );
+              });
+            return this;
+        }
 }
 export default sellScaled;
