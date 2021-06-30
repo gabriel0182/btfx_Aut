55cb1f444934accf40133590cc37c9ad71b955bb
Julian Villabona <jrva93@gmail.com>
2021-06-29 22:23:34 -0500

    Refactor createLongPosition

--- cypress/integration/PageObject/longPosition.js
+++ cypress/integration/PageObject/longPosition.js
@@ -1,186 +1,47 @@
 class longPosition {
-<<<<<<< HEAD
-  addPosition() {
-    const tradingTab = cy.get(
-      ".header__nav-buttons-wrapper > .header__nav-trading"
-    );
-    tradingTab.click()
-    const add = cy
-      .get(
-        '[style="display: flex; align-items: baseline;"] > .trigger > .ui-button'
-      )
-      .get(
-        '[style="display: flex; align-items: baseline;"] > .trigger > .ui-button > .fa'
-      )
-      .scrollIntoView();
-    add.click();
-    return this;
-  }
-  requiredInfo() {
-    const testData = require("../../fixtures/positions.json");
-    testData.forEach((testDataRow) => {
-      const data = {
-        type2: testDataRow.type2,
-        amount: testDataRow.amount,
-      };
-      context(`Generating a test for ${data.type2}`, () => {
-        const selectType = cy.waitUntil(() =>
-        cy
-          .get(".ui-modaldialog__body")
-          .get(".themed-react-select__value-container")
-          .should('be.visible')
-        )
-        selectType.type(`${data.type2}{enter}{enter}`).blur({force:true})
-        const Long = cy.waitUntil(() =>
-          cy
-            .get(".ui-radioinput > :nth-child(1)")
-            .get(":nth-child(1) > .circle")
-            .should("be.visible")
-            .click({force:true})
-        );
-        const positionAmount = cy.waitUntil(() =>
-        cy.get(".increase-positon__input")
-        .type(data.amount).wait(1000)
-        )
-      })
-      const proceed = cy.waitUntil(() =>
-        cy
-          .get(".increase-position-modal > .ui-button")
-          .should("be.visible")
-          .click()
-      );
-    });
-    return this;
-  }
-  successMsg() {
-    const msg = cy.waitUntil(() =>
-      cy
-        .get(".notification-text__text")
-        .should("be.visible")
-        .should("contain", "Submitting position increase")
-    );
-    return this;
-  }
-  cancelPosition() {
-    const testData = require("../../fixtures/positions.json");
-    testData.forEach((testDataRow) => {
-      const data = {
-        amount: testDataRow.amount,
-      };
-      context(`Generating a test for ${data.amount}`, () => {
-        const positionsTable = cy
-          .get('[style="height: 25px; width: 100%;"] > .table-vir__row')
-          .get("div")
-          .first()
-          .each(($div) => {
-            cy.get(
-              '[style="flex: 0 1 110px;"] > div > :nth-child(1) > .ui-button'
-            );
-            cy.get(
-              '[style="flex: 0 1 110px;"] > div > :nth-child(1) > .ui-button > .fa'
-            ).click();
-          });
-        const confirm = cy
-          .get(".ui-modaldialog__footer")
-          .get(".ui-modaldialog__footer > .ui-button--green");
-        confirm.click();
-        const msgCancel = cy.waitUntil(() =>
-          cy
-            .get(".notification-text__text")
-            .should("be.visible")
-            .should(
-              "contain",
-              `Margin market sell order of ${data.amount} BTC has been fully executed`
-            )
-        );
-      });
-    });
-    return this;
-  }
-=======
-	addPosition() {
-		const tradingTab = cy.get('.header__nav-buttons-wrapper > .header__nav-trading')
-		tradingTab.click()
-		const add = cy
-			.get('[style="display: flex; align-items: baseline;"] > .trigger > .ui-button')
-			.get('[style="display: flex; align-items: baseline;"] > .trigger > .ui-button > .fa')
-			.scrollIntoView()
-		add.click()
-		return this
-	}
-	requiredInfo() {
-		const testData = require('../../fixtures/positions.json')
-		testData.forEach(testDataRow => {
-			const data = {
-				type: testDataRow.type,
-				amount: testDataRow.amount,
-			}
-			context(`Generating a test for ${data.type}`, () => {
-				const selectType = cy.waitUntil(() =>
-					cy
-						.get('.ui-modaldialog__body')
-						.get('.themed-react-select__value-container')
-						.should('be.visible')
-				)
-				selectType.type(`${data.type}{enter}{enter}`).blur({ force: true })
-				const Long = cy.waitUntil(() =>
-					cy
-						.get('.ui-radioinput > :nth-child(1)')
-						.get(':nth-child(1) > .circle')
-						.should('be.visible')
-						.click({ force: true })
-				)
-				const positionAmount = cy.waitUntil(() =>
-					cy.get('.increase-positon__input').type(data.amount).wait(1000)
-				)
-			})
-			const proceed = cy.waitUntil(() =>
-				cy.get('.increase-position-modal > .ui-button').should('be.visible').click()
-			)
+	static addPosition() {
+		cy.get('[data-qa-id="header-link-trading"]').filter('.header__nav-button').click()
+		cy.get('[data-qa-id="positions"]').within(() => {
+			cy.get('[type="button"]').first().click()
 		})
-		return this
 	}
-	successMsg() {
-		const msg = cy.waitUntil(() =>
-			cy
-				.get('.notification-text__text')
-				.should('be.visible')
-				.should('contain', 'Submitting position increase')
-		)
-		return this
-	}
-	cancelPosition() {
-		const testData = require('../../fixtures/positions.json')
-		testData.forEach(testDataRow => {
-			const data = {
-				amount: testDataRow.amount,
-			}
-			context(`Generating a test for ${data.amount}`, () => {
-				const positionsTable = cy
-					.get('[style="height: 25px; width: 100%;"] > .table-vir__row')
-					.get('div')
-					.first()
-					.each($div => {
-						cy.get('[style="flex: 0 1 110px;"] > div > :nth-child(1) > .ui-button')
-						cy.get('[style="flex: 0 1 110px;"] > div > :nth-child(1) > .ui-button > .fa').click()
+
+	static requiredInfo() {
+		cy.fixture('positions').then((position) => {
+			context(`Generating a test for ${position[0].type}`, () => {
+				cy.get('[data-qa-id="modal-dialog-content"]').within(() => {
+					cy.contains('Select').type(`${position[0].type}{enter}{enter}`)
+					cy.get('.ui-radioinput').within(() => {
+						cy.contains('Long').click()
 					})
-				const confirm = cy
-					.get('.ui-modaldialog__footer')
-					.get('.ui-modaldialog__footer > .ui-button--green')
-				confirm.click()
-				const msgCancel = cy.waitUntil(() =>
-					cy
-						.get('.notification-text__text')
-						.should('be.visible')
-						.should(
-							'contain',
-							`Margin market sell order of ${data.amount} BTC has been fully executed`
-						)
-				)
+					cy.contains('Amount').next().type(position[0].amount)
+					cy.contains('Proceed').click()
+				})
 			})
 		})
-		return this
 	}
->>>>>>> d7aa51117b68ed0dac758b1b49ab1398dca4d687
+
+	static successMsg() {
+		const apiUrlStaging = 'https://api.staging.bitfinex.com/v2/auth/r'
+		cy.intercept('POST', `${apiUrlStaging}/stats/rank:vol:3h:tBTCUSD/hist?limit=1`).as('stats')
+		cy.wait('@stats').its('response.statusCode').should('eq', 200)
+		cy.get('.notification-text__text').should('contain', 'Submitting position increase')
+	}
+
+	static cancelPosition() {
+		cy.fixture('positions').then((position) => {
+			context(`Generating a test for ${position[0].amount}`, () => {
+				cy.get('[data-qa-id="positions-table-row"]').within(() => {
+					cy.get('[type="button"]').first().click()
+				})
+				cy.get('[data-qa-id="modal-dialog"]').within(() => {
+					cy.get('[data-qa-id="modal-dialog-action-button"]').contains('Okay').click()
+				})
+				cy.wait('@stats').its('response.statusCode').should('eq', 200)
+				const confirmationMsg = `Margin market sell order of ${position[0].amount} BTC has been fully executed`
+				cy.get('.notification-text__text').should('contain', confirmationMsg)
+			})
+		})
+	}
 }
 export default longPosition
--- cypress/integration/createLongPosition/createLongPosition.js
+++ cypress/integration/createLongPosition/createLongPosition.js
@@ -4,7 +4,6 @@ import login from '../PageObject/login.js'
 import longPosition from '../PageObject/longPosition.js'
 
 const staging = new login()
-const pstl = new longPosition()
 
 Given('I go to Trading page', () => {
 	staging.landing()
@@ -12,14 +11,14 @@ Given('I go to Trading page', () => {
 })
 
 When('I select the Create a New Long Position option', () => {
-	pstl.addPosition()
+	longPosition.addPosition()
 })
 
 When('I fill out all the required fields', () => {
-	pstl.requiredInfo()
+	longPosition.requiredInfo()
 })
 
 Then('I verify the position was created', () => {
-	pstl.successMsg()
-	pstl.cancelPosition()
+	longPosition.successMsg()
+	longPosition.cancelPosition()
 })
