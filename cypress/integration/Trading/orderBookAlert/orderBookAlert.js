a753bb1e7f21552cdea4f6d2c2ebbc39353d83b7
Julian Villabona <jrva93@gmail.com>
2021-08-18 20:49:09 -0500

    Variable name changed to bidAsk

--- cypress/integration/Trading/orderBookAlert/orderBookAlert.js
+++ cypress/integration/Trading/orderBookAlert/orderBookAlert.js
@@ -3,12 +3,12 @@
 import notification from '../../../support/PageObject/notification'
 import orderBook from '../../../support/PageObject/orderBook'
 
-When('I click on the bell icon within an Order Book {string} entry to add an alert', (type) => {
-	orderBook.addAlert(type)
+When('I click on the bell icon within an Order Book {string} entry to add an alert', (bidAsk) => {
+	orderBook.addAlert(bidAsk)
 })
 
-When('I click on the bell icon within an Order Book {string} entry to del an alert', (type) => {
-	orderBook.removeAlert(type)
+When('I click on the bell icon within an Order Book {string} entry to del an alert', (bidAsk) => {
+	orderBook.removeAlert(bidAsk)
 })
 
 Then('I get an alert containing: {string}', (notificationMessage) => {
@@ -17,10 +17,10 @@ Then('I get an alert containing: {string}', (notificationMessage) => {
 	notification.closeNotification()
 })
 
-And('A bell icon is displayed within the Order Book {string} entry', (type) => {
-	orderBook.bellIconIsDisplayed(type)
+And('A bell icon is displayed within the Order Book {string} entry', (bidAsk) => {
+	orderBook.bellIconIsDisplayed(bidAsk)
 })
 
-And('The bell icon is hidden within the Order Book {string} entry', (type) => {
-	orderBook.bellIconIsHidden(type)
+And('The bell icon is hidden within the Order Book {string} entry', (bidAsk) => {
+	orderBook.bellIconIsHidden(bidAsk)
 })
