dadaae4cfcc528e8c33cc4ee493d0db53cc63570
garreth-sutton <garreth.sutton@bitfinex.com>
2021-09-05 01:24:20 +0100

    Improved token management; implemented on-disk token caching

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,11 +13,46 @@
 // the project's config changing)
 
 const cucumber = require('cypress-cucumber-preprocessor').default
+const fs = require('fs')
+const tokenFile = 'token.txt'
+const axios = require('axios').default
 
 module.exports = (on, config) => {
 	on('file:preprocessor', cucumber())
 	on('task', {
 		generateOTP: require('cypress-otp'),
+		readTokenFromFileAndCheckValidity() {
+			if (fs.existsSync(tokenFile)) {
+				// Check the on-disk cache for a token value
+				// If present, attempt a token recycle, this tests if the token is still valid
+				const token = fs.readFileSync(tokenFile, 'utf8')
+				const renewTokenUrl = 'https://api.staging.bitfinex.com/v2/auth/w/token'
+				const renewBody = { scope: 'api', writePermission: true, caps: ['o', 'f', 's', 'w'] }
+				return axios
+					.post(renewTokenUrl, renewBody, {
+						headers: {
+							'Content-Type': 'application/json',
+							'bfx-token': decodeURIComponent(token),
+						},
+					})
+					.then((response) => {
+						if (response.status == 200) {
+							const token = response.data[0]
+							fs.writeFile(tokenFile, token, 'utf8')
+							return token
+						}
+						return null
+					})
+					.catch((error) => {
+						return null
+					})
+			}
+			return null
+		},
+		writeTokenToFile(token) {
+			fs.writeFile(tokenFile, token, 'utf8')
+			return null
+		},
 	})
 	on('before:browser:launch', (browser, launchOptions) => {
 		if (browser.name === 'chrome') {
