591a25685fa452d69ffccc047567f732553fc518
gabriel0182 <gabriel0182@gmail.com>
2021-09-07 16:17:16 -0500

    added script for Trades Yours Buy

---
+++ cypress/integration/Trading/TradesYoursBuy.feature
@@ -0,0 +1,15 @@
+Feature: Trades Yours Buy
+
+    @loginBitfinex
+    Scenario: Trades Yours Buy
+
+        Given I am viewing the trading page
+        When I switch to the Yours tab on the Trades component
+         When I select "Market" order
+        And  I place an Exchange Market Buy order
+        Then A row is added to the Trades table
+        And It has a "Green" background colour
+        And It contains the trade amount
+        And It contains the trade price
+        And it contains the trade time
+        And It contains a "Green" "Up" icon
--- cypress/integration/Trading/TradesYoursBuy/TradesYoursBuy.js
+++ cypress/integration/Trading/TradesYoursBuy/TradesYoursBuy.js
@@ -1,5 +1,3 @@
-///  <reference types="cypress"/>
-
 import orderForm from '../../../support/PageObject/orderForm'
 import trades from '../../../support/PageObject/trades'
 
@@ -7,8 +5,11 @@ When('I select {string} order', (orderType) => {
 	orderForm.selectOrderType(orderType)
 })
 
-And('I place an Exchange Market Sell order', () => {
-	orderForm.sellMarketOrder()
+And('I switch to the Yours tab on the Trades component', () => {
+	trades.switchToYoursTab()
+})
+And('I place an Exchange Market Buy order', () => {
+	orderForm.buyMarketOrder()
 })
 
 Then('A row is added to the Trades table', () => {
@@ -24,13 +25,13 @@ Then('It contains the trade amount', () => {
 })
 
 Then('It contains the trade price', () => {
-	trades.containsTradeSellPrice()
+	trades.containsTradeBuyPrice()
 })
 
 Then('it contains the trade time', () => {
 	trades.containsTradeTime()
 })
 
-Then('it contains a {string} {string} icon', (iconColor,icon) => {
-	trades.validateTradeUpDownIcon(iconColor,icon)
+Then('It contains a {string} {string} icon', (iconColor, icon) => {
+	trades.validateTradeUpDownIcon(iconColor, icon)
 })
